e61be89b419301db6bf5d7bce9263a40a4fd9c5a
Syed Muhammad Mujtaba Naqvi <123009259+Mujtaba315@users.noreply.github.com>
2024-07-22 01:13:38 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header';
+import Note from './components/Note';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Note />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
